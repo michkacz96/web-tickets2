44bae8c54b980c023af7efafae01bb3a42d02750
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-03 16:05:59 +0200

    Created migration for SupportTicket

---
+++ database/migrations/2023_07_03_132317_create_support_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('support_tickets', function (Blueprint $table) {
+            $table->uuid();
+            $table->timestamps();
+            $table->foreignId('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreignId('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('closed_by');
+            $table->foreign('closed_by')->references('id')->on('users')->onDelete('cascade');
+            $table->text('description');
+            $table->string('customer_contact_type', 1); //E - email, P - phone
+            $table->string('customer_contact', 15);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('support_tickets');
+    }
+};
