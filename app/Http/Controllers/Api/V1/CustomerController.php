71d5ae88888033141ef4a6c10bd4d7ef0dae816d
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-04 16:23:36 +0200

    Updated CustomerController with extension of BaseApiController and updated responses in methods

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\V1\BaseApiController;
 use App\Http\Requests\V1\CustomerRequests\StoreCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 
-class CustomerController extends Controller
+class CustomerController extends BaseApiController
 {
     /**
      * Display a listing of the resource.
      */
     public function index(){
-        return new CustomerCollection(Customer::all());
+        $customers = Customer::all();
+
+        return $this->successResponse('Customers retrived successfully. Number of resources: '.count($customers), new CustomerCollection($customers));
     }
 
     /**
@@ -27,14 +29,14 @@ class CustomerController extends Controller
             return response()->noContent();
         }
 
-        return new CustomerResource($customer);
+        return $this->successResponse('Customer retrived successfully', new CustomerResource($customer));
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreCustomerRequest $request){
-        return new CustomerResource(Customer::create($request->all()));
+        return $this->successResponse('Customer created successfully', new CustomerResource(Customer::create($request->all())));
     }
 
     /**
@@ -48,7 +50,7 @@ class CustomerController extends Controller
         }
 
         if($customer->delete()){
-            return response('Resource deleted succesfully');
+            return $this->successResponse('Customer deleted successfully', null);
         }
     }
 }
