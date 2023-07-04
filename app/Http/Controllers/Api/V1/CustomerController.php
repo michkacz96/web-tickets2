511a71702d0343075846015e76f1eb46bdf2bdf9
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-04 14:36:51 +0200

    Created method store to save new customers and added comments

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\CustomerRequests\StoreCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 
 class CustomerController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(){
         return new CustomerCollection(Customer::all());
     }
 
+    /**
+     * Display the specified resource.
+     */
     public function show(int $id){
         $customer = Customer::find($id);
 
@@ -21,5 +28,12 @@ class CustomerController extends Controller
         }
 
         return new CustomerResource($customer);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerRequest $request){
+        return new CustomerResource(Customer::create($request->all()));
     }
 }
