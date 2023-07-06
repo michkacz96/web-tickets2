d2dfbb60181d17018ce23623722e885edf0774cf
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-06 16:32:44 +0200

    Added update method with put and path request methods

--- app/Http/Controllers/Api/V1/CustomerEmailController.php
+++ app/Http/Controllers/Api/V1/CustomerEmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Api\V1\BaseApiController;
 use App\Http\Requests\V1\CustomerEmailRequests\StoreCustomerEmailRequest;
+use App\Http\Requests\V1\CustomerEmailRequests\UpdateCustomerEmailRequest;
 use App\Http\Resources\V1\CustomerEmailCollection;
 use App\Http\Resources\V1\CustomerEmailResource;
 use App\Models\CustomerEmail;
@@ -45,9 +46,16 @@ class CustomerEmailController extends BaseApiController
     /**
      * Update the specified resource in storage.
      */
-    public function update()
+    public function update(UpdateCustomerEmailRequest $request, int $id)
     {
-        //
+        $email = CustomerEmail::find($id);
+
+        if(is_null($email)){
+            return $this->errorResponse('Bad Request', 400);
+        }
+
+        $email->update($request->all());
+        return $this->successResponse('Customer\'s email updated successfully', new CustomerEmailResource($email));
     }
 
     /**
