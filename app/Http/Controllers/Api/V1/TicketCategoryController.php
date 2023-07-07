b2c9635b8a60fe6be1b7632c80db92f43dfa393b
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-07 16:13:01 +0200

    Added methods index - show all resources, show - show only selected resource and store to save to database new ticket categories

--- app/Http/Controllers/Api/V1/TicketCategoryController.php
+++ app/Http/Controllers/Api/V1/TicketCategoryController.php
@@ -2,33 +2,43 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\V1\BaseApiController;
+use App\Http\Requests\V1\TicketCategoryRequests\StoreTicketCategoryRequest;
+use App\Http\Resources\V1\TicketCategoryCollection;
+use App\Http\Resources\V1\TicketCategoryResource;
 use App\Models\TicketCategory;
 
-class TicketCategoryController extends Controller
+class TicketCategoryController extends BaseApiController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $categories = TicketCategory::all();
+        return $this->successResponse('Ticket categories retrived successfully. Number of resources: '.count($categories), new TicketCategoryCollection($categories));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store()
+    public function store(StoreTicketCategoryRequest $request)
     {
-        //
+        return $this->successResponse('Ticket category created successfully', new TicketCategoryResource(TicketCategory::create($request->all())));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show()
+    public function show(int $id)
     {
-        //
+        $category = TicketCategory::find($id);
+
+        if(is_null($category)){
+            return response()->noContent();
+        }
+
+        return $this->successResponse('Ticket category retrived successfully', new TicketCategoryResource($category));
     }
 
     /**
