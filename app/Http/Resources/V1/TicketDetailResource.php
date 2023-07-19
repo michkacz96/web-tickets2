f01fcfe91444b0793cd59c4c2aa82d2040aa06c7
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-19 16:41:41 +0200

    Created TicketDetail Resource and Collection

---
+++ app/Http/Resources/V1/TicketDetailResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->uuid,
+            'ticketId' => $this->ticket_id,
+            'action' => $this->action,
+            'type' => $this->type,
+            'message' => $this->message,
+            'userId' => $this->user_id,
+        ];
+    }
+}
