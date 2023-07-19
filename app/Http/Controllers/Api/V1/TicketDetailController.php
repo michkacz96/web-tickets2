083d5bb3d8a071d635de4cc9e806a16f197728af
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-19 16:26:02 +0200

    Created TicketDetailController

---
+++ app/Http/Controllers/Api/V1/TicketDetailController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Api\V1\BaseApiController;
+use App\Models\TicketDetail;
+
+class TicketDetailController extends BaseApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update()
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        //
+    }
+}
