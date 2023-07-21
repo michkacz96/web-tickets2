2317cf4dd30ea63fd2f7d35f8f1aa363945d5df1
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-21 10:05:13 +0200

    Updated TicketDetail Model

--- app/Models/TicketDetail.php
+++ app/Models/TicketDetail.php
@@ -2,18 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TicketDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'ticket_id',
         'action',
-        'type',
         'message',
         'user_id',
     ];
