40b47b9d27c128e5296137b7897660197cd3955f
Michał Kaczmarczyk <michkacz96@gmail.com>
2023-07-21 10:52:46 +0200

    Added method index to show all ticket details and method show to present one specified resource

--- app/Http/Controllers/Api/V1/TicketDetailController.php
+++ app/Http/Controllers/Api/V1/TicketDetailController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Api\V1\BaseApiController;
+use App\Http\Requests\V1\TicketDetailRequests\StoreTicketDetailRequest;
+use App\Http\Resources\V1\TicketDetailCollection;
+use App\Http\Resources\V1\TicketDetailResource;
 use App\Models\TicketDetail;
 
 class TicketDetailController extends BaseApiController
@@ -12,23 +15,30 @@ class TicketDetailController extends BaseApiController
      */
     public function index()
     {
-        //
+        $ticketDetails = TicketDetail::all();
+        return $this->successResponse('Tickets details retrived successfully. Number of resources: '.count($ticketDetails), new TicketDetailCollection($ticketDetails));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store()
+    public function store(StoreTicketDetailRequest $request)
     {
-        //
+        return $this->successResponse('Message created successfully', new TicketDetailResource(TicketDetail::create($request->all())));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show()
+    public function show(string $id)
     {
-        //
+        $ticketDetail = TicketDetail::find($id);
+
+        if(is_null($ticketDetail)){
+            return $this->errorResponse("Resource not found");
+        }
+
+        return $this->successResponse('Ticket detail retrived successfully', new TicketDetailResource($ticketDetail));
     }
 
     /**
